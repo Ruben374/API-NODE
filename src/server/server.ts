644160407a9696b92e84e14cf22b72abac78541c
Ruben André <rubandre14@gmail.com>
2025-07-02 17:21:55 +0100

    Initial Config

---
+++ src/server/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+//criar instancia do server
+const server = express();
+
+//configurar o servidor
+
+server.get('/', (req:any, res:any) => {
+  return res.send("ola dev");
+});
+
+export { server };
